0c24780a6d7caa608b7fe9158855201a9c2ce95b
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-09-01 19:53:54 +0600

    added login

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from "./components/pages/auth/Login/login";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+function App () {
+  return(
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
-
-export default App
---
+++ src/components/pages/auth/Login/login.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../../../firebase'; 
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import './Login.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSignIn = async (event) => {
+    event.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log('User logged in:', user.uid);
+      navigate('/'); 
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="login-card-container">
+      <div className="login-card">
+        <div className="login-card-logo">
+          <img src="logo.gif" alt="logo" />
+        </div>
+        <div className="login-card-header">
+          <h1>Sign In</h1>
+          <div>Please login to use the platform</div>
+        </div>
+        <form className="login-card-form" onSubmit={handleSignIn}>
+          <div className="form-item">
+            <span className="form-item-icon material-symbols-rounded"></span>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoFocus
+              required
+            />
+          </div>
+          <div className="form-item">
+            <span className="form-item-icon material-symbols-rounded"></span>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          {error && <p className="error-message">{error}</p>}
+          <div className="form-item-other">
+            <div className="checkbox">
+              <input type="checkbox" id="rememberMeCheckbox" defaultChecked />
+              <label htmlFor="rememberMeCheckbox">Remember me</label>
+            </div>
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+        <div className="login-card-footer">
+          Don't have an account? <a href="/signup">Create a free account.</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
