6e345ecb7b4877161afbc228b9124e380cfe390b
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-09-03 02:52:02 +0600

    chnges in auth

--- src/components/pages/auth/Login/login.jsx
+++ src/components/pages/auth/Login/login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth'; // Import the function
 import logo from '../../../../assets/logo.png';
 import { auth } from '../../../../firebase'; 
 import './login.css';
@@ -24,51 +25,51 @@ const Login = () => {
 
   return (
     <div className="container-login">
-    <div className="login-card-container">
-      <div className="login-card">
-        <div className="login-card-logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <div className="login-card-header">
-          <h1>Sign In</h1>
-          <div>Please login to use the platform</div>
-        </div>
-        <form className="login-card-form" onSubmit={handleSignIn}>
-          <div className="form-item">
-            <span className="form-item-icon material-symbols-rounded"></span>
-            <input
-              type="text"
-              placeholder="Enter Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              autoFocus
-              required
-            />
+      <div className="login-card-container">
+        <div className="login-card">
+          <div className="login-card-logo">
+            <img src={logo} alt="logo" />
           </div>
-          <div className="form-item">
-            <span className="form-item-icon material-symbols-rounded"></span>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+          <div className="login-card-header">
+            <h1>Sign In</h1>
+            <div>Please login to use the platform</div>
           </div>
-          {error && <p className="error-message">{error}</p>}
-          <div className="form-item-other">
-            <div className="checkbox">
-              <input type="checkbox" id="rememberMeCheckbox" defaultChecked />
-              <label htmlFor="rememberMeCheckbox">Remember me</label>
+          <form className="login-card-form" onSubmit={handleSignIn}>
+            <div className="form-item">
+              <span className="form-item-icon material-symbols-rounded"></span>
+              <input
+                type="text"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoFocus
+                required
+              />
             </div>
+            <div className="form-item">
+              <span className="form-item-icon material-symbols-rounded"></span>
+              <input
+                type="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            {error && <p className="error-message">{error}</p>}
+            <div className="form-item-other">
+              <div className="checkbox">
+                <input type="checkbox" id="rememberMeCheckbox" defaultChecked />
+                <label htmlFor="rememberMeCheckbox">Remember me</label>
+              </div>
+            </div>
+            <button type="submit">Sign In</button>
+          </form>
+          <div className="login-card-footer">
+            Don't have an account? <a href="/signup">Create a free account.</a>
           </div>
-          <button type="submit">Sign In</button>
-        </form>
-        <div className="login-card-footer">
-          Don't have an account? <a href="/signup">Create a free account.</a>
         </div>
       </div>
-    </div>
     </div>
   );
 };
